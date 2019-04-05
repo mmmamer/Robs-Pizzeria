1715b7baae189d191c2796c7032baff74e8800d6
mmmamer <mo_amer@live.co.uk>
2019-04-05 14:31:12 +0100

    Organisation of files

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+// Event handling
+// document.addEventListener("DOMContentLoaded",
+//   function (event) {
+    
+//     // Unobtrusive event binding
+//     document.querySelector("button")
+//       .addEventListener("click", function () {
+//         // Call server to get the name
+//         $ajaxUtils
+//           .sendGetRequest("data/name.json", 
+//             function (res) {
+//               var message = 
+//                 res.firstName + " " + res.lastName
+//               if (res.likesChineseFood) {
+//                 message += " likes Chinese food";
+//               }
+//               else {
+//                 message += " doesn't like Chinese food";
+//               }
+//               message += " and uses ";
+//               message += res.numberOfDisplays + 1;
+//               message += " displays for coding.";
+
+//               document.querySelector("#content")
+//                 .innerHTML = "<h2>" + message + "</h2>";
+//             });
+//       });
+//   }
+// );
+
+// var btn1 = document.getElementById("button1");
+// var btn2 = document.getElementById("button2");
+// var btn3 = document.getElementById("button3");
+// var mov = document.querySelector(".mover");
+
+// btn1.addEventListener('click', function() {
+//     // change the position of the image
+//     mover.
+// })
+
+// btn2.addEventListener('click', function() {
+  
+// })
+
+// btn3.addEventListener('click', function() {
+  
+// })
+
+// you only forget if you don't reinforce it. I have to pull this out of the depths of my brain. 
+// hack it to together first and then afterwards make it much better
+// improve it after, ack it first improve it later. 
